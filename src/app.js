a52c855525930cb3cac37736e5b33b5262a529b9
Samuel Rinnetmäki <samuel@iki.fi>
2014-09-07 09:02:08 +0300

    Configure multiple locations

--- src/app.js
+++ src/app.js
@@ -5,27 +5,29 @@ var innerR = 52;
 var outerR = 72;
 var centerX = 72;
 var centerY = 72;
-var R = 6371000;
+// var R = 6371000;
+var R = 6378137;
+var YARD_LENGTH = 0.9144;
+var YARDS_IN_MILE = 1760;
+// var divider = 100000;
 var units = 'metric';
-var targetLat;
-var targetLon;
+var targetLoc;
 var myLat;
 var myLon;
 var myHeading;
-
-Settings.config(
-  { url: 'https://rawgit.com/samuelmr/pebble-findcache/master/configure.html' },
-  function(e) {
-    if (e.options && e.options.lat) {
-      targetLat = e.options.lat;
-      targetLon = e.options.lon;
-      units = e.options.units;
-    }
-    updateView();
-  }
-);
-
+var watcher;
 var main = new UI.Window();
+var hint = new UI.Card();
+var menu = new UI.Menu();
+
+if (Settings.option('locations') && Settings.option('locations').length) {
+  buildMenu(Settings.option('locations'));
+}
+else {
+  hint.title('Find Cache');
+  hint.body('Use phone to configure!');
+  hint.show();
+}
 
 var outer = new UI.Circle({ position: new Vector2(centerX, centerY), radius: outerR });
 main.add(outer);
@@ -73,6 +75,12 @@ var west = new UI.Text({
 });
 main.add(west);
 
+var me = new UI.Circle({ radius: 8, position: new Vector2(72, 72) });
+main.add(me);
+
+var target = new UI.Circle({ backgroundColor: 'black', radius: 8, position: new Vector2(72, 72) });
+main.add(target);
+
 var head = new UI.Text({
   position: new Vector2(32, 50),
   size: new Vector2(80, 20),
@@ -93,30 +101,58 @@ var dist = new UI.Text({
 });
 main.add(dist);
 
-var me = new UI.Circle({ radius: 8, position: new Vector2(72, 72) });
-main.add(me);
-
-var target = new UI.Circle({ backgroundColor: 'black', radius: 8, position: new Vector2(72, 72) });
-main.add(target);
-
-main.show();
-
-navigator.geolocation.watchPosition(
-  function(position) {
-    myLat = position.coords.latitude;
-    myLon = position.coords.longitude;
-    myHeading = position.coords.heading;
-    updateView();
-  },
-  function(error) {
-    head.text = 'location';
-    dist.text = 'error';
-  },
-  {enableHighAccuracy: true}
+Settings.config(
+  { url: 'https://rawgit.com/samuelmr/pebble-findcache/master/configure.html' },
+  function(e) {
+    if (e.options && e.options.locations) {
+      units = e.options.units;
+      buildMenu(e.options.locations);
+    }
+    if (hint) {
+      // hint.hide();
+    }
+  }
 );
 
+menu.on('select', function(e) {
+  targetLoc = e.item.subtitle;
+  dist.text('');
+  head.text('');
+  main.show();
+  watcher = navigator.geolocation.watchPosition(
+    function(position) {
+      console.log('Heading to ' + position.coords.heading);
+      // add rounding of current coordinates
+      myHeading = position.coords.heading;
+      myLat = position.coords.latitude;
+      myLon = position.coords.longitude;
+      updateView();
+    },
+    function(error) {
+      head.text('location');
+      dist.text('error');
+    },
+    {enableHighAccuracy: true}
+  );
+});
+
+function buildMenu(locations) {
+  var locs = [];
+  for (var i=0; i<locations.length; i++) {
+    if (locations[i][0] && locations[i][1]) {
+      locs[i] = {title: utf8(locations[i][0]), subtitle: locations[i][1]};
+    }
+  }
+  menu.section(0, {title: 'Locations', items: locs});  
+  menu.show();
+}
+
 function updateView() {
-  if (targetLat || targetLon) {
+  if (targetLoc) {
+    var loc = targetLoc.split(',');
+    var targetLat = loc[0];
+    var targetLon = loc[1];
+    console.log('Showing view to ' + targetLat + ',' + targetLon);
     var dLat = (targetLat-myLat) * Math.PI / 180;
     var dLon = (targetLon-myLon) * Math.PI / 180;
     var l1 = myLat * Math.PI / 180;
@@ -124,24 +160,49 @@ function updateView() {
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(l1) * Math.cos(l2); 
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    dist.text(Math.round(R * c) + ' m');
+    var d = Math.round(R * c);
+    if (units == 'imperial') {
+      d = d / YARD_LENGTH;
+    }
+    var u = (units != 'imperial') ? 'm' : 'yd';
+    console.log('units: ' + u);
+    if (d > 5000) {
+      var divider = (units != 'imperial') ? 1000 : YARDS_IN_MILE;
+      d = Math.round(100 * d / divider)/100;
+      u = (units != 'imperial') ? 'km' : 'mi';
+    }
+    var newdist = d + ' ' + u;
     var y = Math.sin(dLon) * Math.cos(l2);
     var x = Math.cos(l1)*Math.sin(l2) -
             Math.sin(l1)*Math.cos(l2)*Math.cos(dLon);
     var targetHeading = Math.round(Math.atan2(y, x) * 180 / Math.PI);
     targetHeading = targetHeading < 0 ? 360 + targetHeading : targetHeading;
-    head.text(targetHeading + '°');
+    var newhead = targetHeading + '°';
     var targetX = centerX + (innerR + (outerR - innerR)/2) * Math.sin((180 - targetHeading) * Math.PI / 180);
     var targetY = centerY + (innerR + (outerR - innerR)/2) * Math.cos((180 - targetHeading) * Math.PI / 180);
-    target.animate('position', new Vector2(targetX, targetY));
-    var myX = -20;
+    var myX = 72;
     var myY = -20;
     if (myHeading !== null) {
       myX = centerX + (innerR - (outerR - innerR)/2) * Math.sin((180 - myHeading) * Math.PI / 180);
       myY = centerY + (innerR - (outerR - innerR)/2) * Math.cos((180 - myHeading) * Math.PI / 180);  
     }
     myHeading = myHeading < 0 ? 360 + myHeading : myHeading;
-    me.animate('position', new Vector2(myX, myY));
+    if ((newdist != dist.text()) ||
+        (newhead != head.text()) || 
+        (me.position().x != myX) ||
+        (me.position().y != myY) || 
+        (target.position().x != targetX) ||
+        (target.position().y != targetY))  {
+      console.log('update screen');
+      dist.text(newdist);
+      head.text(newhead);
+      target.animate('position', new Vector2(targetX, targetY));
+      me.animate('position', new Vector2(myX, myY));    
+    } 
     // console.log('dist is ' + dist.text() + ' and head is ' + head.text() + '; my heading is ' + myHeading);
   }
+}
+
+function utf8(str) {
+  return unescape(encodeURI(str));
 }
